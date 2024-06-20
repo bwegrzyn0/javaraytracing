04d5a2db402da80d027aa2d9302da848284ac425
bwegrzyn0 <bwegrzyn@duck.com>
2024-06-20 11:59:36 +0200

    anti-aliasing, wiele promieni na piksel

--- src/main/Screen.java
+++ src/main/Screen.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.*;
 import java.awt.image.*;
+import java.util.Random;
 
 import math.*;
 import ray.*;
@@ -48,34 +49,55 @@ public class Screen extends Canvas {
     bufferStrategy.show();
   }
 
+  Random random = new Random();
+  float samplesPerPixelSquare = 5;
+  int samplesPerPixel = (int) (samplesPerPixelSquare * samplesPerPixelSquare);
+
+  public Ray getRay(int i, int j, int k, int l) {
+    Vector3 pixelCenter = new Vector3(pixel00Center);
+    pixelCenter.add(deltaU.multiplied((float) i + (float) k * (1f / samplesPerPixelSquare) - 0.5f));
+    pixelCenter.add(deltaV.multiplied(-(float) j + (float) l * (1f / samplesPerPixelSquare) - 0.5f));
+    Vector3 rayDir = new Vector3(pixelCenter);
+    rayDir.add(cameraCenter.multiplied(-1));
+    rayDir.normalize();
+    return new Ray(rayDir, cameraCenter);
+  }
+
   public void sendRays() {
     for (int i = 0; i < Main.WIDTH; i++) {
       for (int j = 0; j < Main.HEIGHT; j++) {
-        Vector3 pixelCenter = new Vector3(pixel00Center);
-        pixelCenter.add(deltaU.multiplied(i));
-        pixelCenter.add(deltaV.multiplied(-j));
-        Vector3 rayDir = new Vector3(pixelCenter);
-        rayDir.add(cameraCenter.multiplied(-1));
-        rayDir.normalize();
-        Ray ray = new Ray(rayDir, cameraCenter);
-        drawPixel(i, j, rayColor(ray));
+        int[] color = { 0, 0, 0 };
+        for (int k = 0; k < samplesPerPixelSquare; k++) {
+          for (int l = 0; l < samplesPerPixelSquare; l++)
+            for (int m = 0; m < 3; m++) {
+              color[m] += rayColor(getRay(i, j, k, l), samplesPerPixel)[m];
+            }
+        }
+
+        int colorInt = ((color[0] / samplesPerPixel) << 16) + ((color[1] / samplesPerPixel) << 8)
+            + (color[2] / samplesPerPixel);
+        drawPixel(i, j, colorInt);
       }
     }
   }
 
-  public int rayColor(Ray ray) {
-    int color = (ray.dir.y >= 0) ? 255 + (100 << 8) : (100 << 16) + (100 << 8) + 100;
+  public int[] rayColor(Ray ray, float samplePoints) {
+    float blue = 255f;
+    float green = 100f * 0.5f * (1 + ray.dir.y) + 255f * (0.5f - 0.5f * ray.dir.y);
+    float red = 255f * (0.5f - 0.5f * ray.dir.y);
     for (Sphere sphere : Main.spheres) {
       float intersect = sphere.rayIntersection(ray);
       if (intersect >= 0) {
         Vector3 normal = ray.pointAt(intersect);
         normal.add(sphere.position.multiplied(-1));
         normal.normalize();
-        color = ((int) ((1 + normal.x) * 255 / 2) << 16) + ((int) ((1 + normal.y) * 255 / 2) << 8)
-            + (int) ((normal.z + 1) * 255 / 2);
+        red = (1 + normal.x) * 255f / 2f;
+        green = (1 + normal.y) * 255f / 2f;
+        blue = (normal.z + 1) * 255f / 2f;
         break;
       }
     }
+    int[] color = { (int) red, (int) green, (int) blue };
     return color;
   }
 }
